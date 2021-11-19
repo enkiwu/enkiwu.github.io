bb84ef49a441bc8015316d2b47314b3f69994226
qi.wu <260364807@qq.com>
2021-11-19 10:20:51 +0800

    feat: react init

---
+++ README.md
@@ -0,0 +1,4 @@
+## 个人小项目
+
+yarn install && yarn start
+
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        test page
+ 
+      </header>
+    </div>
+  );
+}
+
+export default App;
